ae843116e9e1dbf2999ff24f4307a7ed0f164eb5
Pranav Pillai <pillaip@seas.upenn.edu>
2018-01-20 11:16:10 -0500

    resolved

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,7 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-<<<<<<< HEAD
 var Factory = artifacts.require("./Factory.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(Factory);
-=======
-
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
->>>>>>> acc8e37ddc23c7c05fe7fbde005f028145c8c7af
 };
